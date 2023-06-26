a8507ffb325a8603c59c0140b10d97fcb0810426
Matt <matthew.sheehan94@gmail.com>
2023-06-26 12:47:56 +0100

    add api endpoint and tests

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const app = express()
+const { getEndpoints } = require(`${__dirname}/controllers/api.controllers`)
 const { getTopics } = require(`${__dirname}/controllers/topics.controllers`)
 const { handlePsqlErrors, handleCustomErrors, handleInternalErrors} = require(`${__dirname}/errors/errors`)
+
+app.get("/api", getEndpoints)
 
 app.get("/api/topics", getTopics)
 
