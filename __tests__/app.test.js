59db80599fbe76c9b47de7cb632d6e490677b2ee
Matt <matthew.sheehan94@gmail.com>
2023-06-27 18:07:17 +0100

    add comment_count property to get article by ID endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -113,18 +113,19 @@ describe("GET /api/articles", () => {
 describe("GET /api/articles/:article_id", () => {
     test("article object contains all correct properties and has specified ID", () => {
         return request(app)
-            .get("/api/articles/7")
+            .get("/api/articles/9")
             .expect(200)
             .then(({ body }) => {
                 expect(body.article).toMatchObject({
-                    article_id: 7,
+                    article_id: 9,
                     author: expect.any(String),
                     title: expect.any(String),
                     body: expect.any(String),
                     topic: expect.any(String),
                     created_at: expect.any(String),
                     votes: expect.any(Number),
-                    article_img_url: expect.any(String)
+                    article_img_url: expect.any(String),
+                    comment_count: expect.any(String)
                 })
             })
     })
