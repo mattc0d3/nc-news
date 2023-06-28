2cb87f54589dfffa1abba4ad57f3e02ff6a0e847
Matt <matthew.sheehan94@gmail.com>
2023-06-28 11:58:48 +0100

    update listen.js to get PORT from process.env

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
 const app = require(`${__dirname}/api/app`)
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, (err) => {
+app.listen(PORT, (err) => {
     if (err) console.log(err)
     else console.log("Listening on port 9090...")
 })
